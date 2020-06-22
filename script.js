3654081e0f9ba7fbb2d7caa9ec3c0df5eebd077a
BELZONA <webmaster@belzona.com>
2020-06-21 21:05:51 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -80,17 +80,17 @@ jQuery( function( $ ) {
 				if(val.iso_code == iso && val.formulation_number){
 					docs.push("<div class=\"elementor-element elementor-widget elementor-widget-accordion\"><div class=\"elementor-widget-container\"><div class=\"elementor-accordion\"><div class=\"elementor-accordion-item\"><div class=\"elementor-tab-title\"><a class=\"elementor-accordion-title\">FN" + val.formulation_number + "</a></div><div class=\"elementor-tab-content elementor-clearfix elementor-active\" style=\"display: block;\"><ul clas=\"elementor-icon-list-items\">");
 					if(val.IF){
-		            	docs.push("<li class=\"elementor-icon-list-item\"><a href=\"" + val.IF + "\">Instructions for Use</a></li>");
+		            	docs.push("<li class=\"elementor-icon-list-item\"><a href=\"" + val.IF + "\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-thumbs-up\"></i></span><span class=\"elementor-icon-list-text\">Instructions for Use</span></a></li>");
 		            }
 		            if(val.PSS){
-		            	docs.push("<li class=\"elementor-icon-list-item\"><a href=\"" + val.PSS + "\">Product Specification Sheet</a></li>");
+		            	docs.push("<li class=\"elementor-icon-list-item\"><a href=\"" + val.PSS + "\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-thumbs-up\"></i></span><span class=\"elementor-icon-list-text\">Product Specification Sheet</span></a></li>");
 		            }
 		            if(val.CR){
-		            	docs.push("<li class=\"elementor-icon-list-item\"><a href=\"" + val.CR + "\">Chemical Resistant Chart</a></li>");
+		            	docs.push("<li class=\"elementor-icon-list-item\"><a href=\"" + val.CR + "\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-thumbs-up\"></i></span><span class=\"elementor-icon-list-text\">Chemical Resistant Chart</span></a></li>");
 		            }
 		            if(val.SD){
 		            	$.each(val.SD, function(k,v){
-		                	docs.push("<li class=\"elementor-icon-list-item\"><a href=\"" + v.doc_url + "\">" + v.doc_name + "</a></li>");
+		                	docs.push("<li class=\"elementor-icon-list-item\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-thumbs-up\"></i></span><span class=\"elementor-icon-list-text\"><a href=\"" + v.doc_url + "\">" + v.doc_name + "</span></a></li>");
 		            	});
 		            }
 		            docs.push("</div></div></div></div></div></ul>");
