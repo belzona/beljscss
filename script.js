09dfb5fe9617524d0fb33bc7f6b37aa99801eeac
BELZONA <webmaster@belzona.com>
2020-06-22 14:37:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ jQuery( function( $ ) {
 	/* function to load documents accordion */
 	$.fn.get_docs = function(iso, prod)
 	{
+		$("#docs-downloads").html("");
 		$(".elementor-widget-accordion").css("display", "block");
 		var api_url = "https://bel.belzona.com/assets/xml/productdocsjson.aspx?prod=";
 		$.getJSON( api_url + prod, function(data) {
@@ -83,13 +84,13 @@ jQuery( function( $ ) {
 					} else { docprim = ""; }
 					docs.push("<div id=\"" + docprim + "\" class=\"fn-box elementor-element elementor-widget elementor-widget-accordion\"><div class=\"elementor-widget-container\"><div class=\"elementor-accordion\"><div class=\"elementor-accordion-item\"><div class=\"elementor-tab-title " + docprim + "\"><a class=\"elementor-accordion-title fn-title\">FN" + val.formulation_number + "</a></div><div class=\"elementor-tab-content elementor-clearfix elementor-active elementor-widget-icon-list\" style=\"display: block;\"><ul clas=\"elementor-icon-list-items\">");
 					if(val.IF){
-		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.IF + "\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-download\"></i></span><span class=\"elementor-icon-list-text\">IFU</span></a></li>");
+		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.IF + "\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-lightbulb\"></i></span><span class=\"elementor-icon-list-text\">IFU</span></a></li>");
 		            }
 		            if(val.PSS){
-		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.PSS + "\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-download\"></i></span><span class=\"elementor-icon-list-text\">PSS</span></a></li>");
+		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.PSS + "\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-flask\"></i></span><span class=\"elementor-icon-list-text\">PSS</span></a></li>");
 		            }
 		            if(val.CR){
-		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.CR + "\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-download\"></i></span><span class=\"elementor-icon-list-text\">CRC</span></a></li>");
+		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.CR + "\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-vial\"></i></span><span class=\"elementor-icon-list-text\">CRC</span></a></li>");
 		            }
 		            if(val.SD){
 		            	$.each(val.SD, function(k,v){
