5978a1826d119742d568468045b7f2ffed5416b8
BELZONA <webmaster@belzona.com>
2020-06-22 14:56:51 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -84,17 +84,17 @@ jQuery( function( $ ) {
 					} else { docprim = ""; }
 					docs.push("<div id=\"" + docprim + "\" class=\"fn-box elementor-element elementor-widget elementor-widget-accordion\"><div class=\"elementor-widget-container\"><div class=\"elementor-accordion\"><div class=\"elementor-accordion-item\"><div class=\"elementor-tab-title " + docprim + "\"><a class=\"elementor-accordion-title fn-title\">FN" + val.formulation_number + "</a></div><div class=\"elementor-tab-content elementor-clearfix elementor-active elementor-widget-icon-list\" style=\"display: block;\"><ul clas=\"elementor-icon-list-items\">");
 					if(val.IF){
-		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.IF + "\" download=\"IFU\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-lightbulb\"></i></span><span class=\"elementor-icon-list-text\">IFU</span></a></li>");
+		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.IF + "\" download=\"" + val.formulation_number + "-ifu.pdf\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-lightbulb\"></i></span><span class=\"elementor-icon-list-text\">IFU</span></a></li>");
 		            }
 		            if(val.PSS){
-		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.PSS + "\" download=\"PSS\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-flask\"></i></span><span class=\"elementor-icon-list-text\">PSS</span></a></li>");
+		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.PSS + "\" download=\"" + val.formulation_number + "-pss.pdf\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-flask\"></i></span><span class=\"elementor-icon-list-text\">PSS</span></a></li>");
 		            }
 		            if(val.CR){
-		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.CR + "\" download=\"CRC\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-vial\"></i></span><span class=\"elementor-icon-list-text\">CRC</span></a></li>");
+		            	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + val.CR + "\" download=\"" + val.formulation_number + "-crc.pdf\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-vial\"></i></span><span class=\"elementor-icon-list-text\">CRC</span></a></li>");
 		            }
 		            if(val.SD){
 		            	$.each(val.SD, function(k,v){
-		                	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + v.doc_url + "\" download=\"SDS\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-exclamation-triangle\"></i></span><span class=\"elementor-icon-list-text\">" + v.doc_name + "</span></a></li>");
+		                	docs.push("<li class=\"elementor-icon-list-item\"><a target=\"_new\" href=\"" + v.doc_url + "\" download=\"" + val.formulation_number + "-sds.pdf\"><span class=\"elementor-icon-list-icon\"><i aria-hidden=\"true\" class=\"fas fa-exclamation-triangle\"></i></span><span class=\"elementor-icon-list-text\">" + v.doc_name + "</span></a></li>");
 		            	});
 		            }
 		            docs.push("</div></div></div></div></div></ul>");
