3512dbc27199fb99a16fa51c4bdab48937c5f6a4
BELZONA <webmaster@belzona.com>
2021-04-01 13:53:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,73 +9,9 @@ jQuery( function( $ ) {
 	if($(".single-product_information").length){
 		$("#menu-item-95").addClass("current-menu-ancestor current-menu-parent");
 	}
-/* icons for search results */
-/**** removed for proper styling
-	if($(".search").length || $(".archive").length){
-		if($(".type-belzona_report").length){
-			$(".type-belzona_report").each(function(index){
-				var thelnk = $(this).find("a").attr("href");
-				$('<a>',{
-					class: 'elementor-post__thumbnail__link no-lightbox',
-					href: thelnk,
-					html: '<div class=\"icon_type\"><i aria-hidden=\"true\" class=\"fas fa-chart-bar\"></i></div>'
-				}).prependTo($(this));
-			});
-		}
-		if($(".type-approval").length){
-			$(".type-approval").each(function(index){
-				var thelnk = $(this).find("a").attr("href");
-				$('<a>',{
-					class: 'elementor-post__thumbnail__link no-lightbox',
-					href: thelnk,
-					html: '<div><i aria-hidden=\"true\" class=\"fas fa-thumbs-up\"></i></div>'
-				}).prependTo($(this));
-			});
-		}
-		if($(".type-testing_report").length){
-			$(".type-testing_report").each(function(index){
-				var thelnk = $(this).find("a").attr("href");
-				$('<a>',{
-					class: 'elementor-post__thumbnail__link no-lightbox',
-					href: thelnk,
-					html: '<div><i aria-hidden=\"true\" class=\"fas fa-list-alt\"></i></div>'
-				}).prependTo($(this));
-			});
-		}
-		if($(".type-technical_mailing").length){
-			$(".type-technical_mailing").each(function(index){
-				var thelnk = $(this).find("a").attr("href");
-				$('<a>',{
-					class: 'elementor-post__thumbnail__link no-lightbox',
-					href: thelnk,
-					html: '<div><i aria-hidden=\"true\" class=\"fas fa-paper-plane\"></i></div>'
-				}).prependTo($(this));
-			});
-		}
-		if($(".type-system_leaflet").length){
-			$(".type-system_leaflet").each(function(index){
-				var thelnk = $(this).find("a").attr("href");
-				$('<a>',{
-					class: 'elementor-post__thumbnail__link no-lightbox',
-					href: thelnk,
-					html: '<div><i aria-hidden=\"true\" class=\"fas fa-camera-retro\"></i></div>'
-				}).prependTo($(this));
-			});
-		}
-		if($(".type-commercial_update").length){
-			$(".type-commercial_update").each(function(index){
-				var thelnk = $(this).find("a").attr("href");
-				$('<a>',{
-					class: 'elementor-post__thumbnail__link no-lightbox',
-					href: thelnk,
-					html: '<div><i aria-hidden=\"true\" class=\"fas fa-globe\"></i></div>'
-				}).prependTo($(this));
-			});
-		}
-	}
-*******/
 	/* languages for product documentation */
 	if($(".single-product_information").length){
+		alert($("#belapiurl").text());
 		var prod = $("#prod_number").val();
 		$.getJSON( "https://bel.belzona.com/assets/xml/productdocsjsonMD5.aspx?langs=all", function(data) {
 			 var items = [];
