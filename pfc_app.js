4a4de9ffa7ea8f790374b88532545131e17d6fb0
BELZONA <webmaster@belzona.com>
2022-04-19 15:27:25 +0100

    Add files via upload
    product fatigue calculator - Javascript

---
+++ pfc_app.js
@@ -0,0 +1,112 @@
+function getJSON(JSONurl, callback) {
+    var request = new XMLHttpRequest();
+
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            // Success!
+            data = JSON.parse(request.responseText);
+            console.log(data);
+        } else {
+            // We reached our target server, but it returned an error
+            if (request.status == 401) {
+                //window.location.replace("/account/index");
+            }
+        }
+        callback(data);
+    };
+
+    request.onerror = function () {
+        // There was a connection error of some sort
+    };
+
+    request.open('GET', JSONurl, true);
+    request.send();
+}
+
+
+/** Validate the search form inputs */
+var validateSearchCriteria = function () {
+
+    var errMsgs = [];
+
+    var pid = document.getElementById("productid");
+    if (pid) {
+        if (pid.value == '0') { errMsgs[errMsgs.length] = "Product"; }
+    }
+
+    var cycles = document.getElementById('cycles');
+    if (cycles) {
+        if (cycles.value == '') { errMsgs[errMsgs.length] = "No of Cycles"; }
+        else {
+            if (isNaN(Number(cycles.value))) { errMsgs[errMsgs.length] = "No. of Cycles, wrong format"; return errMsgs; }
+
+            if (cycles.value < 10000 || cycles.value > 10000000 ) { errMsgs[errMsgs.length] = "Cycles must be between 10000 - 10000000"; return errMsgs; }
+        }
+    }
+    return errMsgs;
+}
+
+var getFatigueStressVals = function (e) {
+
+    e.preventDefault();
+    resultsJSON = null;
+    var divResults = document.getElementById("divResults");
+    divResults.innerHTML = '';
+
+    //Validate the form
+    var formErrors = validateSearchCriteria();
+
+    var pId = document.getElementById('productid').value;
+    var cycles = document.getElementById('cycles').value;
+
+    //Check if the validation returned any errors
+    if (formErrors.length == 0) {
+
+        var form = document.getElementById("frmSearch");
+        if (form) {
+
+            getJSON("https://api.belzona.com/Product/GetFatigueStressValues/" + pId + "/" + cycles + "/", function (data) {
+            //getJSON("http://localhost:9476/Product/GetFatigueStressValues/" + pId + "/" + cycles + "/", function (data) {
+            //getJSON("http://localhost:5015/Product/GetFatigueStressValues/" + pId + "/" + cycles + "/", function (data) {
+                resultsJSON = data;
+                var resultsHTML = '';
+                if (resultsJSON && resultsJSON.constructor === Array) {
+                    for (var i = 0; i < resultsJSON.length; i++) {
+                        resultsHTML += "<div class=\"col-md-4 mb-4\">";
+                        resultsHTML += "<strong>Product:&nbsp;</strong> " + resultsJSON[i]["ProductName"] + "<br/>";
+                        resultsHTML += "<strong>No. of Cycles:&nbsp;</strong> " + resultsJSON[i]["NumberOfCycles"] + "<br/>";
+                        resultsHTML += "<strong>Frequency:&nbsp;</strong><span class='text-danger font-weight-bold	'> " + resultsJSON[i]["TestFrequency"] + " </span><span class='small'>(Hz)</span><br/>";
+                        resultsHTML += "<strong>Mean Stress:&nbsp;</strong> " + resultsJSON[i]["MeanStress"] + " <span class='small'>(MPa)</span><br/>";
+                        resultsHTML += "<strong>Stress Amplitude:&nbsp;</strong> " + resultsJSON[i]["StressAmplitutde"] + " <span class='small'>(MPa)</span><br/>";
+                        resultsHTML += "<strong>Lower Stress:&nbsp;</strong><span class='text-danger font-weight-bold'>" + resultsJSON[i]["LowerStress"] +  " </span><span class='small'>(MPa)</span><br/>";
+                        resultsHTML += "<strong>Upper Stress:&nbsp;</strong><span class='text-danger font-weight-bold'> " + resultsJSON[i]["UpperStress"] + " </span><span class='small'>(MPa)</span><br/>";
+                        resultsHTML += "</div>";
+                    }
+                }
+                if (divResults) {
+                    divResults.innerHTML = resultsHTML;
+                }
+
+            });
+        }
+
+    } else {
+        var errMsgs = "";
+        let errLen = formErrors.length;
+        for (var i = 0; i < errLen; i++) {
+            errMsgs += "* " + formErrors[i] + "<br />";
+        }
+        divResults.innerHTML = "<div class=\"errors\" id=\"divErrors\" style=\"color:#ff0000 !important;\">" + errMsgs + "</div>";
+    }
+}
+
+
+var setupSearchListener = function () {
+    var element = document.getElementById("btnSearch");
+    //Add a event listener for the download buttton
+    if (element) {
+        element.addEventListener('click', getFatigueStressVals, false);
+    }
+}
+
+setupSearchListener();
